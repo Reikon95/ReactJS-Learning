e88d30f8697e900296b4607edcc43905da31929a
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-02 14:33:24 +0100

    Update basics.js

--- basics.js
+++ basics.js
@@ -15,3 +15,8 @@ const p2 = <p id="small">bar</p>
       
 
 // JSX ELEMENTS ARE BASICALLY HTML IN JAVASCRIPT
+
+const myDiv = <div><h1>Hello world</h1></div> 
+//   nested element
+  
+// a JSX expression must have exactly one outermost element. You can just wrap everything in a div
